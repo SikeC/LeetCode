513e8152cd83eef85f558c808cac60e3c3b4a8b2
SikeC <1263686918@qq.com>
2018-03-22 16:49:46 +0800

    using 10ms

---
+++ 083RemoveDuplicatesfromSortedList/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+  public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *tmp;
+        if (head == NULL)
+            return head;
+        tmp = head;
+        while (tmp->next != NULL && tmp!= NULL)
+        {
+            if (tmp->next->val == tmp->val)
+                tmp->next = tmp->next->next;
+        }
+        return head;
+    }
+};
+
+int main()
+{
+    return 0;
+}
