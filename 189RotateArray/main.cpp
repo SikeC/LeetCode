0b19bcbc93a03bb73b64546c85982e6fe1be3363
SikeC <1263686918@qq.com>
2018-04-12 15:50:17 +0800

    using 20ms

---
+++ 189RotateArray/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+  public:
+    void rotate(vector<int> &nums, int k)
+    {
+        int n = nums.size();   
+        k=n-k%n;
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
