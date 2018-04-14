4436beddc345c9474b10a0345325a8eaf513fa94
SikeC <1263686918@qq.com>
2018-04-14 14:10:28 +0800

    using 0ms

---
+++ 198HouseRobber/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+  public:
+    int rob(vector<int> &nums)//用动态规划思想，每一个数都是包括或者不包括，如果包括那么就是它和i-2之前的最优来，不包括，它就是i-1那个
+    {
+        int length=nums.size();
+        int i;
+        if(length == 0)
+        {
+            return 0;
+        }
+        if(length==1)
+        {
+            return nums[0];
+        }
+        if(length==2)
+        {
+            return nums[0]>nums[1]?nums[0]:nums[1];
+        }
+        nums[0]>nums[1]?nums[1]=nums[0]:nums[1]=nums[1];
+        for(i=2;i<length;i++)
+        {
+            nums[i-1]>nums[i-2]+nums[i]?nums[i]=nums[i-1]:nums[i]+=nums[i-2];
+        }
+        return nums[i-1]>nums[i-2]?nums[i-1]:nums[i-2];
+    }
+};
