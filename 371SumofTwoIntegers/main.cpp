9014726045f5c19d6a90f947de921b74f532bade
CaoYue <1263686918@qq.com>
2018-05-20 16:32:45 +0800

    using 0ms

---
+++ 371SumofTwoIntegers/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if(b == 0) return a;        // when carry is zero, return  
+        int sum = a ^ b;        // calculate sum without carry  
+        int carry = (a & b) << 1;       // calculate carry  
+        return getSum(sum, carry);  
+    }
+};
