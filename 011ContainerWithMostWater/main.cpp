2913bdcf8ce18bda6acaebc272647ebe81fc28c0
CaoYue <1263686918@qq.com>
2018-07-10 13:49:44 +0800

    usingv16ms

--- 011ContainerWithMostWater/main.cpp
+++ 011ContainerWithMostWater/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Solution {
  public:
-  int maxArea(vector<int>& height) {
+  int maxArea(vector<int>& height) {//左右双指针，然后哪边低往高的那边靠近
     int left, right, area;
     if (height.size()<2) {
       return -1;
